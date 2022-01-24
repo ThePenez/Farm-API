4807fbada0307decb152683a7046813ecba1606a
Jovan Penezic <jovan.penezic@gmail.com>
2022-01-24 16:24:37 +0100

    Added prototypes for Building routes, small quality of life changes
    List, create and delete Buildings functionalities added, along with a custom error class and middleware for wrapping asynchronous functions with a trycatch

--- controllers/buildings.js
+++ controllers/buildings.js
@@ -1,27 +1,44 @@
 const { StatusCodes } = require('http-status-codes');
+const { createCustomError } = require('../errors/custom-error');
+const asyncWrapper = require('../middleware/async-wrapper')
 const { Building } = require('../models');
 
-const getAllBuildings = async (req, res) => {
+const getAllBuildings = asyncWrapper(async (req, res) => {
   const buildings = await Building.findAll({ raw: true, attributes: ['name', 'unitType', 'numberOfUnits'] });
   console.log(buildings);
   res.status(StatusCodes.OK).json(buildings);
-};
+});
 
-const getBuilding = async (req, res) => {
+const getBuilding = asyncWrapper(async (req, res) => {
   res.send('get building');
-};
+});
 
-const createBuilding = async (req, res) => {
-  res.send('create building');
-};
+const createBuilding = asyncWrapper(async (req, res) => {
+  const { name, unitType } = req.body;
+  const building = {
+    name,
+    unitType,
+    numberOfUnits: 0,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
 
-const updateBuilding = async (req, res) => {
+  const result = Building.create(building);
+  res.status(StatusCodes.CREATEDK).json(result);
+});
+
+const updateBuilding = asyncWrapper(async (req, res) => {
   res.send('update building');
-};
+});
 
-const deleteBuilding = async (req, res) => {
-  res.send('delete building');
-};
+const deleteBuilding = asyncWrapper(async (req, res, next) => {
+  const { id: buildingID } = req.params;
+  const result = await Building.destroy({ where: { id: buildingID } });
+  if (!result) {
+    return next(createCustomError(`No building with id : ${buildingID}`, 404));
+  }
+  res.status(200).json({ result });
+});
 
 module.exports = {
   getAllBuildings,
